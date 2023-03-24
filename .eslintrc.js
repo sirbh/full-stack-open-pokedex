159c2f9c6208e8b0986c430a5c786086846e7ff4
saurabh chauhan <sourabh1202@gmail.com>
2023-03-24 17:51:32 +0200

    added lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,12 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
-    "node": true
+    "node": true,
+    "cypress/globals": true
   },
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:cypress/recommended"
   ],
   "parserOptions": {
     "ecmaFeatures": {
