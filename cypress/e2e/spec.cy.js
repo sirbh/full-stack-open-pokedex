0d18f29f51200191c70335038ba8cb2825b97598
saurabh chauhan <sourabh1202@gmail.com>
2023-03-24 18:26:36 +0200

    added more e2e test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,5 +3,10 @@ describe('Pokedex', function() {
     cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  }),
+  it('Pokemon called Charmander can be opened and has right content', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
   })
 })
