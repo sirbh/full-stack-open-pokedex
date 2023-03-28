f57a3edb3a21a2dab356eb5431e995a8bad4b2a1
saurabh chauhan <sourabh1202@gmail.com>
2023-03-28 14:22:52 +0300

    fixed test

--- src/PokemonPage.jsx
+++ src/PokemonPage.jsx
@@ -33,7 +33,7 @@ const PokemonPage = ({ previous, next }) => {
     <>
       <div className="links">
 
-        {previous && <Link to={`/pokemon/${previous.name}`}>Prev</Link>}
+        {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
         <Link to="/">Home</Link>
         {next && <Link to={`/pokemon/${next.name}`}>Next</Link>}
       </div>
