177dff1b41f0b4bbfc05659225122ba1a8ff937e
saurabh chauhan <sourabh1202@gmail.com>
2023-03-28 19:28:11 +0300

    added periodic helath url

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(express.static('dist'))
 app.get('/health', (req, res) => {
   res.send('ok')
 })
+app.get('/periodic_health', (req, res) => {
+  res.send('ok')
+})
 app.listen(PORT, () => {
   //eslint-disable-next-line no-console
   console.log('server started on port 5000')
